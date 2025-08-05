b75e8bc02467fd476a2385e1a019c2d0229da52a
tayeb-a11 <125749271+tayeb-a11@users.noreply.github.com>
2025-08-05 17:47:05 +0200

    Update web.php

--- erp/routes/web.php
+++ erp/routes/web.php
@@ -5,3 +5,10 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// Routes pour la vérification d'abonnement
+Route::prefix('subscription')->group(function () {
+    Route::get('/check', [\App\Http\Controllers\ERPSubscriptionController::class, 'checkSubscription'])->name('subscription.check');
+    Route::get('/status', [\App\Http\Controllers\ERPSubscriptionController::class, 'getSubscriptionStatus'])->name('subscription.status');
+    Route::get('/expired', [\App\Http\Controllers\ERPSubscriptionController::class, 'redirectToExpiredPage'])->name('subscription.expired');
+});
